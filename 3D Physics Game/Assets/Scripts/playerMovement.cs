3e65d561e356afe87287a29273c9c64c782a287a
SugarRR <rankinr1@montclair.edu>
2025-02-24 20:00:52 -0500

    Edit 2

--- 3D Physics Game/Assets/Scripts/playerMovement.cs
+++ 3D Physics Game/Assets/Scripts/playerMovement.cs
@@ -2,18 +2,42 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour
 {
-
-    public float speed = 2;
+    Rigidbody rb;
+    public float speed = 0.5f;
+    public int score = 0;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(Vector3.forward * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(Vector3.back * speed);
+        }
+
+        /* if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.left * speed);
+        }*/
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * speed);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
     {
         
     }
